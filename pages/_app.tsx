a8aac75a92991684d74673567e735039a8edbcc8
Guy Meiri <guymeiri312@gmail.com>
2022-04-05 17:37:05 +0300

    a lot of stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { Fragment } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"true"} />
+        <link href="https://fonts.googleapis.com/css2?family=Underdog&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />;
+    </Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,27 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import TestComponent from "../components/TestComponent";
-import { createElement, Fragment, ReactChildren } from "react";
-import { GetStaticProps, GetStaticPaths, GetServerSideProps, InferGetStaticPropsType } from "next";
+import { createElement, ReactChildren, useState } from "react";
+import { GetStaticProps, InferGetStaticPropsType } from "next";
+import styles from "./index.module.css";
+import ToggleTheme from "../components/ToggleTheme";
 
-interface IndexProps {
-  children: ReactChildren;
-  guy: string;
-}
 const Home: NextPage = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
-  return (
-    <div style={{ position: "relative", height: "300px" }}>
-      <div>{props.host}</div>
+  const [dogUrl, setDogUrl] = useState<string | null>(null);
 
-      <Image src={props.dogImageUrl} alt="dick move" layout="fill" objectFit="contain" quality={100} />
-      <TestComponent />
+  const onClickHandler = async () => {
+    const res: Response = await fetch("https://dog.ceo/api/breeds/image/random");
+    const data = (await res.json()) as DogApiResponse;
+    setDogUrl(data.message);
+  };
+
+  return (
+    <div className={styles.container}>
+      <ToggleTheme />
+      <img className={styles.image} src={dogUrl ? dogUrl : props.dogImageUrl} alt="dick move" />
+      <button className={styles.refresh} onClick={onClickHandler}>
+        This dog is shit, get a new one
+      </button>
     </div>
   );
 };
@@ -30,7 +35,6 @@ export default Home;
 export const getStaticProps: GetStaticProps = async (context) => {
   const res: Response = await fetch("https://dog.ceo/api/breeds/image/random");
   const data = (await res.json()) as DogApiResponse;
-  const imgElem = createElement("img");
 
   return {
     props: {
