1adbb068605d3ea1571447b6583b0d13ac0d45d2
Guy Meiri <guymeiri312@gmail.com>
2022-04-05 18:52:40 +0300

    adding meta data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"true"} />
         <link href="https://fonts.googleapis.com/css2?family=Underdog&display=swap" rel="stylesheet" />
+        <meta property="og:site_name" content="Dog Fetcher app" />
+        <meta property="og:title" content="Dog Fetcher app" />
+        <meta property="og:description" content="We fetch dogs so you don't have to!" />
+        <meta property="og:image" itemProp="image" content="https://next-demo-dogs.vercel.app/dog.svg" />
+        <meta property="og:type" content="website" />
       </Head>
       <Component {...pageProps} />;
     </Fragment>
